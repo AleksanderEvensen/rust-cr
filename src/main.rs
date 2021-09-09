24e54c2314015da0a7960d70ac527ba1a700d3f8
Aleksander Evensen <ahse03@gmail.com>
2021-09-09 19:08:44 +0200

    Changing font style

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,11 @@
 #[allow(non_snake_case)]
 mod Console;
 
-use std::{thread, time::Duration};
-
 use Console::ConsoleRenderer;
-
-
-
 
 fn main() {
 
-    let mut console = ConsoleRenderer::new("Hello World - Game", 100,40);
+    let mut console = ConsoleRenderer::new("Hello World - Game", 100,100, 10,10);
     let is_running = true;
 
     let mut x = 0.0;
@@ -24,13 +19,12 @@ fn main() {
 
         let (x,y) = get_point_from_angle(x, 10.0);
 
-        console.draw(50+x.round() as i16, 20+y.round() as i16, '*', 0x00f0);
+        console.draw(50+x.round() as i16, 50+y.round() as i16, '*', 0x00f0);
 
-        // console.fill(0,0,5,25,'#', 0x00f0 | 0x0001);
+        console.fill(0,0,5,25,'#', 0x00f0 | 0x0001);
 
         console.blit();
         
-        // thread::sleep(Duration::from_millis(1));
     }
 }
 
